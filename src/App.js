9a9129ace4483f0619dc30eddabd27aa1195bea9
JuwelShaikh <juwelshaikh04@gmail.com>
2023-12-24 16:22:56 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Contens from './Components/Contents';
+import Contents from './Components/Contents';
 
 function App() {
   return (
     <div className="App">
-      <Contens></Contens>
+      <Contents></Contents>
     </div>
   );
 }
