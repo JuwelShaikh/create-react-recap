096aec893d9e8664f27c8a7d6ee6f9e37066430b
JuwelShaikh <juwelshaikh04@gmail.com>
2023-12-24 16:29:48 +0600

    style added

--- src/Components/Contents.js
+++ src/Components/Contents.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const Contents = () => {
+    const contentStyle = {
+        backgroundColor: 'lightblue',
+        padding: '2px',
+        borderRadius: '10px'
+
+        
+    }
     return (
         <div>
-            
+            <h1>Some Contents</h1>
+            <div style={contentStyle}>
+                <h3>Product Name:</h3>
+                <p>Price:</p>
+                <button>Bid the price</button>
+            </div>
         </div>
     );
 };
